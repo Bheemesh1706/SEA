336aa463624ab4baeece325cd4fac1966bc4a3a4
bheemesh1706 <bheemeshbala.71@gmail.com>
2023-05-29 14:52:57 +0530

    Added the model to the scene

--- src/script.js
+++ src/script.js
@@ -3,60 +3,105 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import waterVertexShader from './shaders/water/vertex.glsl'
 import waterFragmentShader from './shaders/water/fragment.glsl'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI({ width: 340 })
-const debugObject={}
+const debugObject = {}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
+
+
+/**
+ * Boat Model
+ */
+const gltfLoader = new GLTFLoader();
+let boatObject=null
+gltfLoader.load('/models/st_maria/scene.gltf',
+    (gltf) => {
+        gltf.scene.scale.set(0.1,0.1,0.1);
+        boatObject=gltf.scene;
+        scene.add(gltf.scene)
+    },
+    (process) => {
+        console.log('Progress')
+        console.log(process)
+    }, (error) => {
+        console.log('error')
+        console.log(error)
+    });
+
+if(boatObject)
+{
+    console.log(boatObject)
+}
+    
 /**
  * Water
  */
 // Geometry
-const waterGeometry = new THREE.PlaneGeometry(2, 2, 512, 512)
+const waterGeometry = new THREE.PlaneGeometry(4, 5, 512, 512)
 //Color
 debugObject.depthColor = '#186691'
-debugObject.surfaceColor='#9bd8ff'
+debugObject.surfaceColor = '#9bd8ff'
 
 // Materials
 const waterMaterial = new THREE.ShaderMaterial({
     vertexShader: waterVertexShader,
     fragmentShader: waterFragmentShader,
-    uniforms:{
-        uBigWavesElevation:{
-            value:0.2
+    uniforms: {
+        uBigWavesElevation: {
+            value: 0.2
         },
-        uBigWavesFrequency:{
-            value: new THREE.Vector2(4,1.5)
+        uBigWavesFrequency: {
+            value: new THREE.Vector2(4, 1.5)
         },
-        uTime:{
-            value:0
+        uTime: {
+            value: 0
         },
         uSmallWavesElevation: { value: 0.15 },
         uSmallWavesFrequency: { value: 3 },
         uSmallWavesSpeed: { value: 0.2 },
         uSmallIterations: { value: 4 },
-        uBigWavesSpeed:{
-            value:0.75
+        uBigWavesSpeed: {
+            value: 0.75
         },
-        uDepthColor:{
+        uDepthColor: {
             value: new THREE.Color(debugObject.depthColor)
         },
-        uSurfaceColor:{
+        uSurfaceColor: {
             value: new THREE.Color(debugObject.surfaceColor)
         },
-        uColorOffset:{
-            value:0.08
+        uColorOffset: {
+            value: 0.08
         },
-        uColorMultiplier:{
-            value:5
+        uColorMultiplier: {
+            value: 5
         }
     }
 })
@@ -69,16 +114,16 @@ scene.add(water)
 
 //Debug Panel 
 
-gui.add(waterMaterial.uniforms.uBigWavesElevation,'value').min(0).max(1).step(0.001).name('uBigWavesElevation');
-gui.add(waterMaterial.uniforms.uBigWavesFrequency.value,'x').min(0).max(10).step(0.001).name('uBigWavesFrequencyX');
-gui.add(waterMaterial.uniforms.uBigWavesFrequency.value,'y').min(0).max(5).step(0.001).name('uBigWavesFrequencyY');
-gui.add(waterMaterial.uniforms.uBigWavesSpeed,'value').min(0).max(5).step(0.001).name('uBigWavesSpeed');
-gui.add(waterMaterial.uniforms.uColorOffset,'value').min(0).max(1).step(0.001).name('uColorOffset');
-gui.add(waterMaterial.uniforms.uColorMultiplier,'value').min(0).max(10).step(0.001).name('uColorMultiplier');
-gui.addColor(debugObject,'depthColor').name('depthColor').onFinishChange(()=>{
+gui.add(waterMaterial.uniforms.uBigWavesElevation, 'value').min(0).max(1).step(0.001).name('uBigWavesElevation');
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'x').min(0).max(10).step(0.001).name('uBigWavesFrequencyX');
+gui.add(waterMaterial.uniforms.uBigWavesFrequency.value, 'y').min(0).max(5).step(0.001).name('uBigWavesFrequencyY');
+gui.add(waterMaterial.uniforms.uBigWavesSpeed, 'value').min(0).max(5).step(0.001).name('uBigWavesSpeed');
+gui.add(waterMaterial.uniforms.uColorOffset, 'value').min(0).max(1).step(0.001).name('uColorOffset');
+gui.add(waterMaterial.uniforms.uColorMultiplier, 'value').min(0).max(10).step(0.001).name('uColorMultiplier');
+gui.addColor(debugObject, 'depthColor').name('depthColor').onFinishChange(() => {
     waterMaterial.uniforms.uDepthColor.value.set(debugObject.depthColor);
 })
-gui.addColor(debugObject,'surfaceColor').name('surfaceColor').onFinishChange(()=>{
+gui.addColor(debugObject, 'surfaceColor').name('surfaceColor').onFinishChange(() => {
     waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor);
 })
 gui.add(waterMaterial.uniforms.uSmallWavesElevation, 'value').min(0).max(1).step(0.001).name('uSmallWavesElevation')
@@ -94,8 +139,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -135,10 +179,10 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
     waterMaterial.uniforms.uTime.value = elapsedTime;
+
     // Update controls
     controls.update()
 
